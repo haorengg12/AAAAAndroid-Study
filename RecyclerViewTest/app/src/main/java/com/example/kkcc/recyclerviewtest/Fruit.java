177639fc5541b5f8d6ace816a71835b4d2631c04
haorengg12 <haorengg12@foxmail.com>
2017-10-10 11:42:54 +0800

    RecyclerView2
    尚未修改完成！

---
+++ RecyclerViewTest/app/src/main/java/com/example/kkcc/recyclerviewtest/Fruit.java
@@ -0,0 +1,23 @@
+package com.example.kkcc.recyclerviewtest;
+
+/**
+ * Created by 黄黄k on 2017-10-09.
+ */
+
+public class Fruit {
+    private String name;
+    private int imageId;
+
+    public Fruit(String name, int imageId) {
+        this.name = name;
+        this.imageId = imageId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getImageId() {
+        return imageId;
+    }
+}
--- RecyclerViewTest/app/src/main/java/com/example/kkcc/recyclerviewtest/FruitAdapter.java
+++ RecyclerViewTest/app/src/main/java/com/example/kkcc/recyclerviewtest/FruitAdapter.java
@@ -1,7 +1,9 @@
 package com.example.kkcc.recyclerviewtest;
 
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.TextView;
 
@@ -13,12 +15,38 @@ import java.util.List;
 
 public class FruitAdapter extends RecyclerView.Adapter<FruitAdapter.ViewHolder> {
     private List<Fruit> mFruitList;
-    static class ViewHolder(View view){
+
+    static class ViewHolder extends RecyclerView.ViewHolder {
         ImageView fruitImage;
         TextView fruitName;
-        public ViewHolder(View view){
+
+        public ViewHolder(View view) {
             super(view);
-            fruitImage=(ImageView)view.findViewById(R.id.frui)
+            fruitImage = (ImageView) view.findViewById(R.id.fruit_image);
+            fruitName = (TextView) view.findViewById(R.id.fruit_name);
         }
+    }
+
+    public FruitAdapter(List<Fruit> fruitList) {
+        mFruitList = fruitList;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.fruit_item, parent, false);
+        ViewHolder holder = new ViewHolder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        Fruit fruit = mFruitList.get(position);
+        holder.fruitImage.setImageResource(fruit.getImageId());
+        holder.fruitName.setText(fruit.getName());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFruitList.size();
     }
 }
