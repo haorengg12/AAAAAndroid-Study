02473a0b6fd3fee4d60c3b0990aa3df064fba565
haorengg12 <haorengg12@foxmail.com>
2017-10-14 17:52:45 +0800

    FragmentBestPractice(未曾解决问题，待解决)

---
+++ FragmentBestPractice/app/src/main/java/com/example/kkcc/fragmentbestpractice/News.java
@@ -0,0 +1,26 @@
+package com.example.kkcc.fragmentbestpractice;
+
+/**
+ * Created by 黄黄k on 2017-10-13.
+ */
+
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
