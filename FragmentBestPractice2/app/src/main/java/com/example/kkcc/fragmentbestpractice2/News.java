79e1bfb5665a99cd6ff5299aa1131e8e88bb6fb4
haorengg12 <haorengg12@foxmail.com>
2017-10-15 11:22:02 +0800

    FragmentBestPractice(FullVersion)

---
+++ FragmentBestPractice2/app/src/main/java/com/example/kkcc/fragmentbestpractice2/News.java
@@ -0,0 +1,29 @@
+package com.example.kkcc.fragmentbestpractice2;
+
+/**
+ * Created by 黄黄k on 2017-10-14.
+ */
+
+public class News {
+
+    private String title;
+
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+}
