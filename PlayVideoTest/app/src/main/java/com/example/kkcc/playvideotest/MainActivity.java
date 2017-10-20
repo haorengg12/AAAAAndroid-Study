9a770fb7a8f908fdce36b4f073b7704389ed3dd9
haorengg12 <haorengg12@foxmail.com>
2017-10-20 22:35:49 +0800

    MediaPlayer

---
+++ PlayVideoTest/app/src/main/java/com/example/kkcc/playvideotest/MainActivity.java
@@ -0,0 +1,93 @@
+package com.example.kkcc.playvideotest;
+
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+import android.widget.VideoView;
+
+import java.io.File;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+    private VideoView videoView;
+    private boolean Pulei = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        videoView = (VideoView) findViewById(R.id.video_view);
+        Button play = (Button) findViewById(R.id.play);
+        play.setOnClickListener(this);
+        Button pause = (Button) findViewById(R.id.pause);
+        pause.setOnClickListener(this);
+        Button replay = (Button) findViewById(R.id.replay);
+        replay.setOnClickListener(this);
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE}, 1);
+        } else {
+            initVideoPath();
+        }
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.play:
+                if (!videoView.isPlaying()) {
+                    videoView.start();
+                    Pulei = true;
+                }
+                break;
+            case R.id.pause:
+                if (videoView.isPlaying()) {
+                    videoView.pause();
+                }
+                break;
+            case R.id.replay:
+                if (videoView.isPlaying() || Pulei) {
+                    videoView.resume();
+                    Pulei = false;
+                }
+                break;
+            default:
+                break;
+        }
+    }
+
+    private void initVideoPath() {
+//        File file = new File(Environment.getExternalStorageDirectory(), "Steam 2017_2_11 18_33_11.mp4");
+        File file = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_MOVIES), "Steam 2017_2_11 18_33_11.mp4");
+        videoView.setVideoPath(file.getPath());
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    initVideoPath();
+                } else {
+                    Toast.makeText(this, "没权限", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+                break;
+            default:
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (videoView != null) {
+            videoView.suspend();
+        }
+    }
+}
