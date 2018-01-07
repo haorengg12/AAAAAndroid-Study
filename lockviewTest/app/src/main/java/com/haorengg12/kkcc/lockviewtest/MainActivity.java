7ca18a1d787885559bcd5daaa180ecba2b9ded7c
haorengg12 <haorengg12@foxmail.com>
2018-01-07 17:50:44 +0800

    完成了图案解锁

---
+++ lockviewTest/app/src/main/java/com/haorengg12/kkcc/lockviewtest/MainActivity.java
@@ -0,0 +1,83 @@
+package com.haorengg12.kkcc.lockviewtest
+        ;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+
+import com.andrognito.patternlockview.PatternLockView;
+import com.andrognito.patternlockview.listener.PatternLockViewListener;
+import com.andrognito.patternlockview.utils.PatternLockUtils;
+import com.andrognito.patternlockview.utils.ResourceUtils;
+
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    private PatternLockView mPatternLockView;
+    private int times = 0;
+
+    private PatternLockViewListener mPatternLockViewListener = new PatternLockViewListener() {
+        @Override
+        public void onStarted() {
+            times++;
+            Log.d(getClass().getName(), "Pattern drawing started && times = " + times);
+        }
+
+        @Override
+        public void onProgress(List<PatternLockView.Dot> progressPattern) {
+            Log.d(getClass().getName(), "Pattern progress: " +
+                    PatternLockUtils.patternToString(mPatternLockView, progressPattern));
+        }
+
+        @Override
+        public void onComplete(List<PatternLockView.Dot> pattern) {
+            //获取最终结果的序列
+            if (times == 3) {
+                //输入错误3次退出程序
+                finish();
+            }
+            if ("13457".equals(PatternLockUtils.patternToString(mPatternLockView, pattern))) {
+                Intent intent = new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(intent);
+                finish();
+            } else {
+                mPatternLockView.setViewMode(PatternLockView.PatternViewMode.WRONG);
+            }
+        }
+
+        @Override
+        public void onCleared() {
+            //点击空白的地方消除
+            Log.d(getClass().getName(), "Pattern has been cleared");
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_main);
+
+        mPatternLockView = (PatternLockView) findViewById(R.id.patter_lock_view);
+        mPatternLockView.setDotCount(3);
+        mPatternLockView.setDotNormalSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_size));
+        mPatternLockView.setDotSelectedSize((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_dot_selected_size));
+        mPatternLockView.setPathWidth((int) ResourceUtils.getDimensionInPx(this, R.dimen.pattern_lock_path_width));
+        mPatternLockView.setAspectRatioEnabled(true);
+        mPatternLockView.setAspectRatio(PatternLockView.AspectRatio.ASPECT_RATIO_HEIGHT_BIAS);
+        mPatternLockView.setViewMode(PatternLockView.PatternViewMode.CORRECT);
+        mPatternLockView.setDotAnimationDuration(150);
+        mPatternLockView.setPathEndAnimationDuration(100);
+        mPatternLockView.setCorrectStateColor(ResourceUtils.getColor(this, R.color.white));
+        mPatternLockView.setInStealthMode(false);
+        mPatternLockView.setTactileFeedbackEnabled(true);
+        mPatternLockView.setInputEnabled(true);
+        mPatternLockView.addPatternLockListener(mPatternLockViewListener);
+    }
+}
